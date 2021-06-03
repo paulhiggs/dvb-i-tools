6859cbe70694252bf91cdcd24ea876f6d0a72bcd
PaulHiggs <paul_higgs@hotmail.com>
2021-06-03 08:09:25 +0100

    update regex according to RFC2141

--- pattern_checks.js
+++ pattern_checks.js
@@ -50,8 +50,8 @@ module.exports.isHTTPURL=function (arg) {
 		'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
 		'((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
 		'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-		'(\\?[\\/?;&a-z\\d%_.~+=-]*)?'+ // query string
-		'(\\#[\\/?-a-z\\d_]*)?$','i'); // fragment locator
+		'(\\?[\\/?;&a-z\\d%_.:~+=-]*)?'+ // query string
+		'(\\#[\\/?;&a-z\\d%_.:~+=-]*)?$','i'); // fragment locator
 	return pattern.test(arg);
 };
 
