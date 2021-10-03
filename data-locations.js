b314150b25ad4a164e347a605bb03eb397898ef5
PaulHiggs <paul_higgs@hotmail.com>
2021-10-03 09:22:08 +0100

    string literals for pathname

--- data-locations.js
+++ data-locations.js
@@ -1,75 +1,75 @@
 // data-locations.js
-
 import { join } from "path";
 
 const REPO_RAW="https://raw.githubusercontent.com/paulhiggs/dvb-i-tools/main/";
 const DVB_METADATA="https://dvb.org/metadata/";
 
+const pathDVBCS='dvb/cs/', pathDVBI="dvbi", pathISO="iso", pathMPEG7="mpeg7", pathTVA="tva",
+      path2007CS=`${pathDVBCS}2007`,
+      path2017CS=`${pathDVBCS}2017`,
+      path2020CS=`${pathDVBCS}2020`,
+      path2021CS=`${pathDVBCS}2021`;
+
 // SLEPR == Service List Entry Point Registry
 const SLEPR_Dir="registries", SLEPR_File="slepr-main.xml";
 export const Default_SLEPR={file:join(SLEPR_Dir,SLEPR_File), url:`${REPO_RAW}${SLEPR_Dir}/${SLEPR_File}`};
 
-const TV_Anytime_dir="tva";
 const idTVA_ContentCS="ContentCS.xml";
-export const TVA_ContentCS={file:join(TV_Anytime_dir, idTVA_ContentCS), url:`${REPO_RAW}${TV_Anytime_dir}/${idTVA_ContentCS}`};
+export const TVA_ContentCS={file:join(pathTVA, idTVA_ContentCS), url:`${REPO_RAW}${pathTVA}/${idTVA_ContentCS}`};
 
 const idTVA_FormatCS="FormatCS.xml";
-export const TVA_FormatCS={file:join(TV_Anytime_dir, idTVA_FormatCS), url:`${REPO_RAW}${TV_Anytime_dir}/${idTVA_FormatCS}`};
+export const TVA_FormatCS={file:join(pathTVA, idTVA_FormatCS), url:`${REPO_RAW}${pathTVA}/${idTVA_FormatCS}`};
 
 const idTVA_PictureCS="PictureFormatCS.xml";
-export const TVA_PictureFormatCS={file:join(TV_Anytime_dir, idTVA_PictureCS), url:`${REPO_RAW}${TV_Anytime_dir}/${idTVA_PictureCS}`};
+export const TVA_PictureFormatCS={file:join(pathTVA, idTVA_PictureCS), url:`${REPO_RAW}${pathTVA}/${idTVA_PictureCS}`};
 
 const idDVB_ContentSubjectCS="DVBContentSubjectCS-2019.xml";
-export const DVBI_ContentSubject={file:join("dvbi", idDVB_ContentSubjectCS), url:`${REPO_RAW}dvbi/${idDVB_ContentSubjectCS}`};
+export const DVBI_ContentSubject={file:join(pathDVBI, idDVB_ContentSubjectCS), url:`${REPO_RAW}${pathDVBI}/${idDVB_ContentSubjectCS}`};
 
 const idDVB_ServiceTypeCS="DVBServiceTypeCS-2019.xml";
-export const DVBI_ServiceTypeCS={file:join("dvbi", idDVB_ServiceTypeCS), url:`${REPO_RAW}dvbi/${idDVB_ServiceTypeCS}`};
+export const DVBI_ServiceTypeCS={file:join(pathDVBI, idDVB_ServiceTypeCS), url:`${REPO_RAW}${pathDVBI}/${idDVB_ServiceTypeCS}`};
 
 const idDVB_AudioCodecCS="AudioCodecCS.xml";
-
 export const DVB_AudioCodecCS={
-    y2007:{file:join("dvb/cs/2007/", idDVB_AudioCodecCS), url:`${DVB_METADATA}cs/2007/${idDVB_AudioCodecCS}`},
-    y2020:{file:join("dvb/cs/2020/", idDVB_AudioCodecCS), url:`${DVB_METADATA}cs/2020/${idDVB_AudioCodecCS}`}
+    y2007:{file:join(path2007CS, idDVB_AudioCodecCS), url:`${DVB_METADATA}${path2007CS}/${idDVB_AudioCodecCS}`},
+    y2020:{file:join(path2020CS, idDVB_AudioCodecCS), url:`${DVB_METADATA}${path2020CS}/${idDVB_AudioCodecCS}`}
 };
 
-
 const idDVB_VideoCodecCS="VideoCodecCS.xml";
-
 export const DVB_VideoCodecCS={
-    y2007:{file:join("dvb/cs/2007", idDVB_VideoCodecCS), url:`${DVB_METADATA}cs/2007/${idDVB_VideoCodecCS}`},
-    y2021:{file:join("dvb/cs/2021", idDVB_VideoCodecCS), url:`${DVB_METADATA}cs/2021/${idDVB_VideoCodecCS}`}
+    y2007:{file:join(path2007CS, idDVB_VideoCodecCS), url:`${DVB_METADATA}${path2007CS}/${idDVB_VideoCodecCS}`},
+    y2021:{file:join(path2021CS, idDVB_VideoCodecCS), url:`${DVB_METADATA}${path2021CS}/${idDVB_VideoCodecCS}`}
 };
 
 const idMPEG_AudioCodingFormatCS="AudioCodingFormatCS.xml";
-export const MPEG7_AudioCodingFormatCS={file:join("mpeg7", idMPEG_AudioCodingFormatCS), url:`${REPO_RAW}mpeg7/${idMPEG_AudioCodingFormatCS}`};
+export const MPEG7_AudioCodingFormatCS={file:join(pathMPEG7, idMPEG_AudioCodingFormatCS), url:`${REPO_RAW}${pathMPEG7}/${idMPEG_AudioCodingFormatCS}`};
 
 const idMPEG7_VisualCodingFormatCS="VisualCodingFormatCS.xml";
-export const MPEG7_VisualCodingFormatCS={file:join("mpeg7", idMPEG7_VisualCodingFormatCS), url:`${REPO_RAW}mpeg7/${idMPEG7_VisualCodingFormatCS}`};
+export const MPEG7_VisualCodingFormatCS={file:join(pathMPEG7, idMPEG7_VisualCodingFormatCS), url:`${REPO_RAW}${pathMPEG7}/${idMPEG7_VisualCodingFormatCS}`};
 
 const idMPEG7_AudioPresentationCS="AudioPresentationCS.xml";
-export const MPEG7_AudioPresentationCS={file:join("mpeg7", idMPEG7_AudioPresentationCS), url:`${REPO_RAW}mpeg7/${idMPEG7_AudioPresentationCS}`};
+export const MPEG7_AudioPresentationCS={file:join(pathMPEG7, idMPEG7_AudioPresentationCS), url:`${REPO_RAW}${pathMPEG7}/${idMPEG7_AudioPresentationCS}`};
 
 const idDVB_AudioConformanceCS="AudioConformancePointsCS.xml";
-export const DVB_AudioConformanceCS={file:join("dvb/cs/2017", idDVB_AudioConformanceCS), url:`${DVB_METADATA}cs/2017/${idDVB_AudioConformanceCS}`};
+export const DVB_AudioConformanceCS={file:join(path2017CS, idDVB_AudioConformanceCS), url:`${DVB_METADATA}${path2017CS}/${idDVB_AudioConformanceCS}`};
 
 const idDVB_VideoConformanceCS="VideoConformancePointsCS.xml";
 export const DVB_VideoConformanceCS={
-    y2017:{file:join("dvb/cs/2017", idDVB_VideoConformanceCS), url:`${DVB_METADATA}cs/2017/${idDVB_VideoConformanceCS}`},
-    y2021:{file:join("dvb/cs/2021", idDVB_VideoConformanceCS), url:`${DVB_METADATA}cs/2021/${idDVB_VideoConformanceCS}`}
+    y2017:{file:join(path2017CS, idDVB_VideoConformanceCS), url:`${DVB_METADATA}${path2017CS}/${idDVB_VideoConformanceCS}`},
+    y2021:{file:join(path2021CS, idDVB_VideoConformanceCS), url:`${DVB_METADATA}${path2021CS}/${idDVB_VideoConformanceCS}`}
 };
 
 const idISO3166="iso3166-countries.json";
-export const ISO3166={file:join("iso", idISO3166), url:`${REPO_RAW}iso/${idISO3166}`};
+export const ISO3166={file:join(pathISO, idISO3166), url:`${REPO_RAW}${pathISO}/${idISO3166}`};
 
 const idDVBI_RecordingInfoCS="DVBRecordingInfoCS-2019.xml";
-export const DVBI_RecordingInfoCS={file:join("dvbi", idDVBI_RecordingInfoCS), url:`${REPO_RAW}dvbi/${idDVBI_RecordingInfoCS}`};
+export const DVBI_RecordingInfoCS={file:join(pathDVBI, idDVBI_RecordingInfoCS), url:`${REPO_RAW}${pathDVBI}/${idDVBI_RecordingInfoCS}`};
 
 const v1Credits="CreditsItem@role-values.txt";
-export const DVBI_CreditsItemRoles={file:join("dvbi", v1Credits), url:`${REPO_RAW}dvbi/${v1Credits}`};
+export const DVBI_CreditsItemRoles={file:join(pathDVBI, v1Credits), url:`${REPO_RAW}${pathDVBI}/${v1Credits}`};
 
 const v2Credits="CreditsItem@role-values-v2.txt";
-export const DVBIv2_CreditsItemRoles={file:join("dvbi", v2Credits), url:`${REPO_RAW}dvbi/${v2Credits}`};
-
+export const DVBIv2_CreditsItemRoles={file:join(pathDVBI, v2Credits), url:`${REPO_RAW}${pathDVBI}/${v2Credits}`};
 
 export const TVAschema={file:join(".", "tva_metadata_3-1.xsd")};
 
