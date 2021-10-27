10dabae09ec109201da6b5da38329a23ae589a10
PaulHiggs <paul_higgs@hotmail.com>
2021-10-27 14:04:31 +0100

    always add forEachSubelement() function to Array

--- cg-check.js
+++ cg-check.js
@@ -261,6 +261,67 @@ function CheckLanguage(validator, errs, lang, loc=null, errCode=null ) {
 }
 
 
+if (!Array.prototype.forEachSubElement) {
+	// based on the polyfill at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach 
+	/*
+	* alternate to Array.prototype.forEach that only returns XML tree nodes that are elements
+	*/
+
+	Array.prototype.forEachSubElement = function(callback, thisArg) {
+
+	if (this == null) { throw new TypeError('Array.prototype.forEachSubElement called on null or undefined'); }
+
+	var T, k;
+	// 1. Let O be the result of calling toObject() passing the
+	// |this| value as the argument.
+	var O = Object(this);
+
+	// 2. Let lenValue be the result of calling the Get() internal
+	// method of O with the argument "length".
+	// 3. Let len be toUint32(lenValue).
+	var len = O.length >>> 0;
+
+	// 4. If isCallable(callback) is false, throw a TypeError exception.
+	// See: https://es5.github.com/#x9.11
+	if (typeof callback !== "function") { throw new TypeError(`${callback} is not a function`); }
+
+	// 5. If thisArg was supplied, let T be thisArg; else let
+	// T be undefined.
+	if (arguments.length > 1) { T = thisArg; }
+
+	// 6. Let k be 0
+	k = 0;
+
+	// 7. Repeat, while k < len
+	while (k < len) {
+
+		var kValue;
+
+		// a. Let Pk be ToString(k).
+		//    This is implicit for LHS operands of the in operator
+		// b. Let kPresent be the result of calling the HasProperty
+		//    internal method of O with argument Pk.
+		//    This step can be combined with c
+		// c. If kPresent is true, then
+		if (k in O) {
+
+		// i. Let kValue be the result of calling the Get internal
+		// method of O with argument Pk.
+		kValue = O[k];
+
+		// ii. Call the Call internal method of callback with T as
+		// the this value and argument list containing kValue, k, and O.
+		if (kValue.type()=='element')
+			callback.call(T, kValue, k, O);
+		}
+		// d. Increase k by 1.
+		k++;
+	}
+	// 8. return undefined
+	};
+}
+
+
 export default class ContentGuideCheck {
 
 	constructor(useURLs, preloadedLanguageValidator=null,  preloadedGenres=null, preloadedCreditItemRoles=null) {
@@ -297,71 +358,6 @@ export default class ContentGuideCheck {
 		this.TVAschema=parseXmlString(readFileSync(TVAschema.file));
 
 		this.supportedRequests=supportedRequests;
-
-		this.extendArray();
-	}
-
-
-	/*private*/ extendArray() {
-		// based on the polyfill at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach 
-		/*
-		* alternate to Array.prototype.forEach that only returns XML tree nodes that are elements
-		*/
-		if (!Array.prototype.forEachSubElement) {
-
-			Array.prototype.forEachSubElement = function(callback, thisArg) {
-		
-			if (this == null) { throw new TypeError('Array.prototype.forEachSubElement called on null or undefined'); }
-		
-			var T, k;
-			// 1. Let O be the result of calling toObject() passing the
-			// |this| value as the argument.
-			var O = Object(this);
-		
-			// 2. Let lenValue be the result of calling the Get() internal
-			// method of O with the argument "length".
-			// 3. Let len be toUint32(lenValue).
-			var len = O.length >>> 0;
-		
-			// 4. If isCallable(callback) is false, throw a TypeError exception.
-			// See: https://es5.github.com/#x9.11
-			if (typeof callback !== "function") { throw new TypeError(`${callback} is not a function`); }
-		
-			// 5. If thisArg was supplied, let T be thisArg; else let
-			// T be undefined.
-			if (arguments.length > 1) { T = thisArg; }
-		
-			// 6. Let k be 0
-			k = 0;
-		
-			// 7. Repeat, while k < len
-			while (k < len) {
-		
-				var kValue;
-		
-				// a. Let Pk be ToString(k).
-				//    This is implicit for LHS operands of the in operator
-				// b. Let kPresent be the result of calling the HasProperty
-				//    internal method of O with argument Pk.
-				//    This step can be combined with c
-				// c. If kPresent is true, then
-				if (k in O) {
-		
-				// i. Let kValue be the result of calling the Get internal
-				// method of O with argument Pk.
-				kValue = O[k];
-		
-				// ii. Call the Call internal method of callback with T as
-				// the this value and argument list containing kValue, k, and O.
-				if (kValue.type()=='element')
-					callback.call(T, kValue, k, O);
-				}
-				// d. Increase k by 1.
-				k++;
-			}
-			// 8. return undefined
-			};
-		}
 	}
 
 
--- sl-check.js
+++ sl-check.js
@@ -117,6 +117,66 @@ function InvalidCountryCode(value, src, loc) {
 }
 
 
+if (!Array.prototype.forEachSubElement) {
+	// based on the polyfill at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach 
+	/*
+	* alternate to Array.prototype.forEach that only returns XML tree nodes that are elements
+	*/
+
+	Array.prototype.forEachSubElement = function(callback, thisArg) {
+
+	if (this == null) { throw new TypeError('Array.prototype.forEachSubElement called on null or undefined'); }
+
+	var T, k;
+	// 1. Let O be the result of calling toObject() passing the
+	// |this| value as the argument.
+	var O = Object(this);
+
+	// 2. Let lenValue be the result of calling the Get() internal
+	// method of O with the argument "length".
+	// 3. Let len be toUint32(lenValue).
+	var len = O.length >>> 0;
+
+	// 4. If isCallable(callback) is false, throw a TypeError exception.
+	// See: https://es5.github.com/#x9.11
+	if (typeof callback !== "function") { throw new TypeError(`${callback} is not a function`); }
+
+	// 5. If thisArg was supplied, let T be thisArg; else let
+	// T be undefined.
+	if (arguments.length > 1) { T = thisArg; }
+
+	// 6. Let k be 0
+	k = 0;
+
+	// 7. Repeat, while k < len
+	while (k < len) {
+
+		var kValue;
+
+		// a. Let Pk be ToString(k).
+		//    This is implicit for LHS operands of the in operator
+		// b. Let kPresent be the result of calling the HasProperty
+		//    internal method of O with argument Pk.
+		//    This step can be combined with c
+		// c. If kPresent is true, then
+		if (k in O) {
+
+		// i. Let kValue be the result of calling the Get internal
+		// method of O with argument Pk.
+		kValue = O[k];
+
+		// ii. Call the Call internal method of callback with T as
+		// the this value and argument list containing kValue, k, and O.
+		if (kValue.type()=='element')
+			callback.call(T, kValue, k, O);
+		}
+		// d. Increase k by 1.
+		k++;
+	}
+	// 8. return undefined
+	};
+}
+
 export default class ServiceListCheck {
 
 	constructor(useURLs, preloadedLanguageValidator=null, preloadedGenres=null, preloadedCountries=null ) {
@@ -163,7 +223,6 @@ export default class ServiceListCheck {
 		this.SLschema_v3=parseXmlString(readFileSync(DVBI_ServiceListSchema.v3.file));
 		this.SLschema_v4=parseXmlString(readFileSync(DVBI_ServiceListSchema.v3x.file));
 
-		this.extendArray();
 		this.loadDataFiles(useURLs);
 	}
 
@@ -215,69 +274,6 @@ export default class ServiceListCheck {
 			return SCHEMA_v1;
 		
 		return SCHEMA_unknown;
-	}
-
-
-	/*private*/ extendArray() {
-		// based on the polyfill at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach 
-		/*
-		* alternate to Array.prototype.forEach that only returns XML tree nodes that are elements
-		*/
-		if (!Array.prototype.forEachSubElement) {
-
-			Array.prototype.forEachSubElement = function(callback, thisArg) {
-		
-			if (this == null) { throw new TypeError('Array.prototype.forEachSubElement called on null or undefined'); }
-		
-			var T, k;
-			// 1. Let O be the result of calling toObject() passing the
-			// |this| value as the argument.
-			var O = Object(this);
-		
-			// 2. Let lenValue be the result of calling the Get() internal
-			// method of O with the argument "length".
-			// 3. Let len be toUint32(lenValue).
-			var len = O.length >>> 0;
-		
-			// 4. If isCallable(callback) is false, throw a TypeError exception.
-			// See: https://es5.github.com/#x9.11
-			if (typeof callback !== "function") { throw new TypeError(`${callback} is not a function`); }
-		
-			// 5. If thisArg was supplied, let T be thisArg; else let
-			// T be undefined.
-			if (arguments.length > 1) { T = thisArg; }
-		
-			// 6. Let k be 0
-			k = 0;
-		
-			// 7. Repeat, while k < len
-			while (k < len) {
-		
-				var kValue;
-		
-				// a. Let Pk be ToString(k).
-				//    This is implicit for LHS operands of the in operator
-				// b. Let kPresent be the result of calling the HasProperty
-				//    internal method of O with argument Pk.
-				//    This step can be combined with c
-				// c. If kPresent is true, then
-				if (k in O) {
-		
-					// i. Let kValue be the result of calling the Get internal
-					// method of O with argument Pk.
-					kValue = O[k];
-			
-					// ii. Call the Call internal method of callback with T as
-					// the this value and argument list containing kValue, k, and O.
-					if (kValue.type()=='element')
-						callback.call(T, kValue, k, O);
-				}
-				// d. Increase k by 1.
-				k++;
-			}
-			// 8. return undefined
-			};
-		}
 	}
 
 
