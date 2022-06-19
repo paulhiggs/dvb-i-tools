1e89417bfd9795cd7c60873b68e7d8cc3364f569
Paul Higgs <paul_higgs@hotmail.com>
2022-06-19 20:10:45 +0100

    write errors as XML comments

--- Validator.js
+++ Validator.js
@@ -61,7 +61,7 @@ export function writeOut(errs, filebase, markup) {
 		outputLines.push(line.value);
 		if (markup && line.validationErrors)
 			line.validationErrors.forEach(error => {
-				outputLines.push(error.replace(/[\n]/g,''));
+				outputLines.push(`<!--${error.replace(/[\n]/g,'')}-->`);
 			});
 	});
 	let filename=markup?`${filebase}.mkup.txt`:`${filebase}.raw.txt`;
