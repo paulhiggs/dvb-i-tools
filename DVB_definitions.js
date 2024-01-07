ec1ca3a152b147838d16969d2042476c76dd4e1d
Paul Higgs <paul_higgs@hotmail.com>
2024-01-07 08:24:21 +0000

    safely initialise members

--- DVB_definitions.js
+++ DVB_definitions.js
@@ -5,19 +5,27 @@ export const dvb = {
 	e_Term: "Term",
 };
 
+const _S_FEC = ["1/2", "3/5", "2/3", "3/4", "4/5", "5/6", "8/9", "9/10"];
+const _S_RollOff = ["0.35"];
+const _S_Modulation = ["QPSK"];
+
+const _S2_RollOff = ["0.25", "0.20"].concat(_S_RollOff);
+const _S2_Modulation = ["8PSK"].concat(_S_Modulation);
+
+const _S2X_RollOff = ["0.15", "0.10", "0.05"].concat(_S2_RollOff);
+const _S2X_FEC = ["7/8", "1/4", "1/3", "2/5", "13/45", "9/20", "11/20", "23/36", "13/18", "5/9", "26/45", "28/45", "7/9", "77/90", "8/15", "32/45", "11/15"].concat(_S_FEC);
+const _S2X_Modulation = ["8PSK-L", "16APSK", "16APSK-L", "32APSK", "32APSK-L", "64APSK", "64APSK-L"].concat(_S2_Modulation);
+
 export const sats = {
 	MODULATION_S: "DVB-S",
 	MODULATION_S2: "DVB-S2",
 	MODULATION_S2X: "DVB-S2X",
-	S_FEC: ["1/2", "3/5", "2/3", "3/4", "4/5", "5/6", "8/9", "9/10"],
-	S_RollOff: ["0.35"],
-	S_Modulation: ["QPSK"],
+	S_FEC: _S_FEC,
+	S_RollOff: _S_RollOff,
+	S_Modulation: _S_Modulation,
+	S2_RollOff: _S2_RollOff,
+	S2_Modulation: _S2_Modulation,
+	S2X_RollOff: _S2X_RollOff,
+	S2X_FEC: _S2X_FEC,
+	S2X_Modulation: _S2X_Modulation,
 };
-
-sats.S2X_FEC = ["7/8", "1/4", "1/3", "2/5", "13/45", "9/20", "11/20", "23/36", "13/18", "5/9", "26/45", "28/45", "7/9", "77/90", "8/15", "32/45", "11/15"].concat(sats.S_FEC);
-
-sats.S2_RollOff = ["0.25", "0.20"].concat(sats.S_RollOff);
-sats.S2X_RollOff = ["0.15", "0.10", "0.05"].concat(sats.S2_RollOff);
-
-sats.S2_Modulation = ["8PSK"].concat(sats.S_Modulation);
-sats.S2X_Modulation = ["8PSK-L", "16APSK", "16APSK-L", "32APSK", "32APSK-L", "64APSK", "64APSK-L"].concat(sats.S2_Modulation);
