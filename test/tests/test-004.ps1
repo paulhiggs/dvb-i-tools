ff70df023f6c2ab3862692e02373b75e8fec9def
Paul Higgs <paul_higgs@hotmail.com>
2025-06-12 16:05:31 +0100

    tests related to parental rating requirements

--- test/tests/test-004.ps1
+++ test/tests/test-004.ps1
@@ -2,4 +2,6 @@ node $PSScriptRoot\..\test-runner.js --mode cg-ProgInfo --nomarkup `
 	"$PSScriptRoot\..\input\test-004\0001r7 DPI.xml" `
 	"$PSScriptRoot\..\input\test-004\0002r7 DPI-empty ReleaseInformation.xml" `
 	"$PSScriptRoot\..\input\test-004\0003r7 DPI-duplicate ReleaseInformation.xml" `
-	"$PSScriptRoot\..\input\test-004\0004r7 DPI-duplicate default ReleaseInformation.xml"
+	"$PSScriptRoot\..\input\test-004\0004r7 DPI-duplicate default ReleaseInformation.xml" `
+	"$PSScriptRoot\..\input\test-004\0005r7 DPI-foreign ParentalRating.xml" `
+	"$PSScriptRoot\..\input\test-004\0006r7 DPI-no age ParentalRating.xml" 
