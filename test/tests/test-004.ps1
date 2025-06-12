08d416ae1b064099e46bc042e2555bebc1b371b8
Paul Higgs <paul_higgs@hotmail.com>
2025-06-12 15:12:11 +0100

    shorten file names and use defined parental guidance values

--- test/tests/test-004.ps1
+++ test/tests/test-004.ps1
@@ -1,5 +1,5 @@
 node $PSScriptRoot\..\test-runner.js --mode cg-ProgInfo --nomarkup `
-	"$PSScriptRoot\..\input\test-004\0001r7 Detailed Program Information Response.xml" `
-	"$PSScriptRoot\..\input\test-004\0002r7 Detailed Program Information Response-empty ReleaseInformation.xml" `
-	"$PSScriptRoot\..\input\test-004\0003r7 Detailed Program Information Response-duplicate ReleaseInformation.xml" `
-	"$PSScriptRoot\..\input\test-004\0004r7 Detailed Program Information Response-duplicate default ReleaseInformation.xml"
+	"$PSScriptRoot\..\input\test-004\0001r7 DPI.xml" `
+	"$PSScriptRoot\..\input\test-004\0002r7 DPI-empty ReleaseInformation.xml" `
+	"$PSScriptRoot\..\input\test-004\0003r7 DPI-duplicate ReleaseInformation.xml" `
+	"$PSScriptRoot\..\input\test-004\0004r7 DPI-duplicate default ReleaseInformation.xml"
